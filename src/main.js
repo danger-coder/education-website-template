d8661b162d08c6ca4715bd82ab98d1dd4d350284
karan chaudhary <karanchy95@gmail.com>
2024-05-25 21:20:36 +0545

    mmenu lirbari cdn added

--- src/main.js
+++ src/main.js
@@ -29,10 +29,14 @@ const swiper = new Swiper(".swiper", {
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 3,
+      slidesPerView: 2,
       spaceBetween: 40,
     },
     1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1200: {
       slidesPerView: 3,
       spaceBetween: 50,
     },
